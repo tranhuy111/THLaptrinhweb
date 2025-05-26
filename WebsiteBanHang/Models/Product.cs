f2f28f499c8fa0465a0882a3dd5b8545785add42
tranhuy111 <tranhuy412004@gmail.com>
2025-05-26 20:45:41 +0700

    Xây dựng website cơ bản với ASP.NET Core MVC

---
+++ WebsiteBanHang/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebsiteBanHang.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+        [Range(0.01, 10000.00)]
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public string? ImageUrl { get; set; } // Đường dẫn đến hình ảnh đại diện
+        public List<string>? ImageUrls { get; set; }
+    }
+}
