91db4e4fb30b59e7336121bf7effa5397317649e
tranhuy111 <tranhuy412004@gmail.com>
2025-06-16 23:21:59 +0700

    baitaptuan6

---
+++ Baitaptuan6/Baitaptuan6/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Baitaptuan6.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>
+        options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
